3275892b6100cbb2c73113bd8eadef77891dc94a
DanglingGem <81005874+DanglingGem@users.noreply.github.com>
2021-07-17 12:26:05 +0100

    commit on main

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const navToggle = document.querySelector(".nav-toggle");
-const openLink = document.querySelector(".link-arrow");
+const openArrow = document.querySelector(".link-arrow");
 const productLink = document.querySelector(".product-links");
 const companyLink = document.querySelector(".company-links");
 const connectLink = document.querySelector(".connect-links");
 
-//console.log();
+//console.log(openLink);
 
-openLink.addEventListener("click", function () {
-    openLink.classList.toggle("show-links");
+openArrow.addEventListener("click", function () {
+   if (openArrow.classList.contains("product-links")) {
+       openArrow.classList.remove("product-links");
+   } else {
+       openArrow.classList.add("product-links");
+   }
+   
 
 });
