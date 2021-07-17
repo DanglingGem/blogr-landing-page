792d818318446db8adc61e70adf73f0bcee33bc8
DanglingGem <81005874+DanglingGem@users.noreply.github.com>
2021-07-17 07:53:18 +0100

    commit on main

---
+++ index.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".nav-toggle");
+const productLink = document.querySelector(".product-links");
+const companyLink = document.querySelector(".company-links");
+const connectLink = document.querySelector(".connect-links");
+
+console.log(navToggle);
+
+navToggle.addEventListener("click", function () {
+    // productLink.classList.toggle("show-links")
+
+});
